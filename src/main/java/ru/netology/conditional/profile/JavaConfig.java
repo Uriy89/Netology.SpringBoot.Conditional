0181fb7ec732c7544f88369ef41e4ab0b9de1dbf
Averianov Yuriy <averianovj@gmail.com>
2022-11-25 19:09:03 +0300

    add param

--- src/main/java/ru/netology/conditional/profile/JavaConfig.java
+++ src/main/java/ru/netology/conditional/profile/JavaConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class JavaConfig {
 
     @Bean
-    @ConditionalOnProperty(name="netology.profile.dev", havingValue="true")
+    @ConditionalOnProperty(name="netology.profile.dev", havingValue="true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
